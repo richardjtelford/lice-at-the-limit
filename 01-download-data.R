9f65af5def0a4af18e2225f77caf53a246f5b04a
RichardJTelford <Richard.Telford@uib.no>
2024-10-28 21:51:44 +0100

    v2

--- 01-download-data.R
+++ 01-download-data.R
@@ -1,6 +1,6 @@
 # download lice count data
 
-# Norway
+#### Norway ####
 
 # URL for data https://www.barentswatch.no/data/fishhealth/lice
 
@@ -13,10 +13,20 @@
 
 # It might be possible to download the data directly through the API, but it might need registration
 
+fs::dir_create("data/norway")
 warning("Please download the norwegian data manually from https://www.barentswatch.no/data/fishhealth/lice and save it to data/norway/lakselus_per_fisk.xlsx")
 
-# Scotland
+#### Scotland ####
 
 # URL for data https://scottishepa.maps.arcgis.com/apps/webappviewer/index.html
-
+fs::dir_create("data/scotland")
 download.file("https://map.sepa.org.uk/sealice/ms_sea_lice.zip", destfile = "data/scotland/ms_sea_lice.zip")
+
+
+#### British Columbia Canada
+
+# URL for data https://www.pac.dfo-mpo.gc.ca/aquaculture/reporting-rapports/index-eng.html
+# https://open.canada.ca/data/en/dataset/3cafbe89-c98b-4b44-88f1-594e8d28838d/resource/f6a948f3-504c-34b0-ac30-58a6b06981e6
+
+fs::dir_create("data/british-columbia")
+download.file("https://api-proxy.edh.azure.cloud.dfo-mpo.gc.ca/catalogue/records/3cafbe89-c98b-4b44-88f1-594e8d28838d/attachments/lice-count-dens-pou-2011-ongoing-rpt-pac-dfo-mpo-aquaculture-eng.csv", destfile = "data/british-columbia/lice-count-dens-pou-2011-ongoing-rpt-pac-dfo-mpo-aquaculture-eng.csv")
---
+++ R/functions.R
@@ -0,0 +1,25 @@
+rdd_wrap <- function(.data, count_var, limit, xlim = c(0, 5), step = 0.01, xlabel = "Number of Adult Female Lice", ...) {
+  counts <- .data |>
+    drop_na({{count_var}}) |>
+#    filter({{count_var}} > 0) |>
+    pull({{count_var}})
+
+
+  jump <-  rddensity(counts, c = limit, p = 3)
+
+  xlim <- c(xlim[1] + step/2, xlim[2] - step/2)
+  plot <- rdplotdensity(jump, counts, plotRange = xlim, histBreaks = seq(xlim[1], xlim[2], by = step), noPlot = TRUE, ylabel = "Number of surveys", xlabel = xlabel, histFillShade = 0.7, ...)
+
+  list(jump = jump, plot = plot)
+
+
+}
+
+
+rdd_extract <- function(jump){
+  tibble(
+    T = jump$test$t_jk,
+    p = jump$test$p_jk
+  )
+
+}
