3ff4581cdcdec4b1046ea3cb44deefa82d4ccce1
RichardJTelford <Richard.Telford@uib.no>
2025-02-28 18:55:23 +0100

    ms upload

--- outreach/render.R
+++ outreach/render.R
@@ -1,1 +1,7 @@
+
 quarto::quarto_render("outreach")
+
+
+fs::file_copy("lice-at-the-limit.pdf", "docs/lice-at-the-limit.pdf", overwrite = TRUE)
+fs::file_copy("lice-at-the-limit.html", "docs/lice-at-the-limit.html", overwrite = TRUE)
+fs::dir_copy("lice-at-the-limit_files", "docs/lice-at-the-limit_files", overwrite = TRUE)
