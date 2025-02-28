ba2ba124dc6c46f79214627fc1dd16e1179405e0
RichardJTelford <Richard.Telford@uib.no>
2025-02-28 23:11:31 +0100

    ensure nojekyll

--- outreach/render.R
+++ outreach/render.R
@@ -1,7 +1,11 @@
-
+# render index, outreach
 quarto::quarto_render("outreach")
 
+# Make _ directories work
+fs::file_create("docs/.nojekyll")
 
+
+# copy ms to docs
 fs::file_copy("lice-at-the-limit.pdf", "docs/lice-at-the-limit.pdf", overwrite = TRUE)
 fs::file_copy("lice-at-the-limit.html", "docs/lice-at-the-limit.html", overwrite = TRUE)
 fs::dir_copy("lice-at-the-limit_files", "docs/lice-at-the-limit_files", overwrite = TRUE)
