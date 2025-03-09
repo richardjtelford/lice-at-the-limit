a6e3f8c7c6773de8364da23bcd023a75cc8943e2
RichardJTelford <Richard.Telford@uib.no>
2025-03-09 16:44:23 +0100

    download ireland. Remove redundant ireland code

--- 01-download-data.R
+++ 01-download-data.R
@@ -11,7 +11,7 @@ fs::dir_create("data/iceland/pdf")
 
 
 # get list of links on arnarlax website
-links <- read_html("https://arnarlax.is/sustainability/quality/") |>
+links_iceland <- read_html("https://arnarlax.is/sustainability/quality/") |>
   html_nodes("a") |>
   html_attr("href") |>
   str_subset("pdf") |>
@@ -23,7 +23,7 @@ links <- read_html("https://arnarlax.is/sustainability/quality/") |>
 
 
 # download pdfs
-for (link in links) {
+for (link in links_iceland) {
   cat("trying", basename(link), "\n")
   if (!file.exists(paste0("data/iceland/pdf/", basename(link)))) {
     tryCatch(
@@ -41,6 +41,8 @@ for (link in links) {
 
 #### Norway ####
 
+fs::dir_create("data/norway")
+
 # URL for data https://www.barentswatch.no/data/fishhealth/lice
 
 # select
@@ -50,12 +52,30 @@ for (link in links) {
 # - excel format
 # The download and put into the data/norway directory
 
-# It might be possible to download the data directly through the API, but it might need registration
+# It should be possible to download the data directly through the API, but it needs authentication. More effort to set up authentication than to download manually.
 
-fs::dir_create("data/norway")
+# norway_download <- httr::GET(
+#   url = "https://www.barentswatch.no/bwapi/v1/geodata/download/fishhealth?",
+#
+#   query = list(
+#     "localityno" = "undefined",
+#     "reporttype" = "lice",
+#     "fromyear" = "2025",
+#     "toyear" = "2025",
+#     "fromweek" = "1",
+#     "toweek" = "10",
+#     "filetype" = "xlsx"
+#   )
+# )
+#
+#
+# # save the file
+# writeBin(norway_download$content, con = "data/norway/lakselus_per_fisk_tmp.xlsx")
+
 message(
   "Please download the norwegian data manually from https://www.barentswatch.no/data/fishhealth/lice and save it to data/norway/lakselus_per_fisk.xlsx"
 )
+
 
 #### Scotland ####
 
@@ -82,199 +102,45 @@ download.file(
 
 # download reports from https://www.marine.ie/site-area/areas-activity/aquaculture/sea-lice/sea-lice
 
-# ireland0 <- read_delim("data/ireland/lice-extracted.csv")
-# ireland_filled <- ireland0 |>
-#   drop_na(data) |>
-#   fill(report, year, bay, company, site, species, cohort) |>
-#   filter(!is.na(data))
-#
-# ireland_filled |>
-#   count(report, year)
-# nrow(ireland_filled)
-#
-# ireland_1 <- ireland_filled |>
-#   filter(is.na(gravid_lep)) |>
-#   filter(is.na(notes)) |>
-#   filter(!str_detect(data, "-")) |>
-#   select(-(gravid_lep:total_cal)) |># arrange(nchar(data)) |> select(-c(report, year, bay, company, species, cohort, notes))
-#   separate_wider_regex(#too_few = "debug",
-#     cols = data,
-#     patterns = c(date = "\\d{2}/\\d{2}/\\d{4}",
-#                    gravid_lep = "\\d+\\.\\d{2}",
-#                 total_lep = "\\d+\\.\\d{2}",
-#                   gravid_cal = "\\d+\\.\\d{2}",
-#                   total_cal = "\\d+\\.\\d{2}")) |>
-#   mutate(date = dmy(date),
-#          self = FALSE,
-#          across(gravid_lep:total_cal, as.numeric))
-#
-#
-# ireland_2 <- ireland_filled |>
-#   filter(!is.na(gravid_lep)) |>
-#   rename(date = data) |>
-#   mutate(date = dmy(date),
-#          self = str_detect(gravid_cal, "-"),
-#          gravid_cal = if_else(str_detect(gravid_cal, "-"), NA, gravid_cal),
-#          gravid_cal = as.numeric(gravid_cal),
-#          self = FALSE)
-#
-# ireland_3 <- ireland_filled |> # covid era data with self counts
-#   filter(is.na(gravid_lep)) |>
-#   filter(is.na(notes)) |>
-#   filter(str_detect(data, "-")) |>
-#   select(-(gravid_lep:total_cal)) |>
-#   separate_wider_regex(#too_few = "debug",
-#     cols = data,
-#     patterns = c(date = "\\d{2}/\\d{2}/\\d{4}",
-#                  gravid_lep = "\\d+\\.\\d{2}",
-#                  total_lep = "\\d+\\.\\d{2}",
-#                  gravid_cal = "-",
-#                  total_cal = "\\d+\\.\\d{2}")) |>
-#
-#   mutate(self = TRUE,
-#     date = dmy(date),
-#     gravid_cal = NA,
-#          across(gravid_lep:total_cal, as.numeric))
-#
-# # combine
-# ireland <- bind_rows(ireland_1, ireland_2, ireland_3) |>
-#   select(-notes)
-#
-#
-#
-# ireland |>
-#   filter(gravid_lep > 0, species == "Atlantic salmon") |>
-#   ggplot(aes(x = gravid_lep, fill = species)) +
-#   geom_histogram(binwidth = 0.01, center = 0.005) +
-#   coord_cartesian(xlim = c(0, 3))
-#
+fs::dir_create("data/ireland")
+fs::dir_create("data/ireland/pdf")
 
-### from pdf
+# Website is erratic. Some links are direct to pdf, others are to a page with a link to the pdf
+# Need to make list of pdf to download manually
 
-pdfs <- tribble(
-  ~file,
-  ~start,
-  ~end,
-  ~good,
-  "Irish Fisheries Bulletin No 33.pdf",
-  NA,
-  NA,
-  FALSE,
-  "Irish Fisheries Bulletin No 45.pdf",
-  25,
-  35,
-  TRUE,
-  "Fisheries Bulletin No. 46.pdf",
-  22,
-  30,
-  TRUE,
-  "Irish Fisheries Bulletin No. 47 - Annual report 2017.pdf",
-  24,
-  34,
-  TRUE,
-  "Irish Fisheries Bulletin No. 48 - Annual report 2018.pdf",
-  25,
-  34,
-  TRUE,
-  "Irish Fisheries Bulletin No. 49 2019.pdf",
-  23,
-  30,
-  TRUE,
-  "Irish Fisheries Bulletin No. 50.pdf",
-  27,
-  36,
-  TRUE,
-  "Irish Fisheries Bulletin No. 52.pdf",
-  26,
-  35,
-  TRUE,
-  "Irish Fisheries Bulletin No40-National Survey of Sea Lice 2011.pdf",
-  26,
-  36,
-  TRUE,
-  "IrishFisheriesBulletin55.pdf",
-  26,
-  37,
-  TRUE,
-  "No 44 2014 National Survey of Sea Lice on Fish Farms in Ireland 2013 (with cover) A4.pdf",
-  25,
-  35,
-  TRUE,
-  "No.41. 2013. National Survey of sea lice on fish farms in Ireland 2012 (with cover) A4.pdf",
-  26,
-  36,
-  TRUE,
-  "No32 Irish Fisheries Bulletin.pdf",
-  27,
-  41,
-  TRUE,
-  "FisheriesBulletinNo.54.pdf",
-  25,
-  34,
-  TRUE,
-  "IFB no. 34.pdf",
-  23,
-  37,
-  TRUE,
-  "IFB No. 53 National survey of Sea lice in Fish farms 2021.pdf",
-  26,
-  35,
-  TRUE
+# get list of links on www.marine.ie website
+links_ireland <- c(
+  `2023` = "https://www.marine.ie/sites/default/files/MIFiles/Docs/Aquaculture/IrishFisheriesBulletin55.pdf",
+  `2022` = "https://www.marine.ie/sites/default/files/MIFiles/Docs/Aquaculture/FisheriesBulletinNo.54.pdf",
+  `2021` = "https://www.marine.ie/sites/default/files/MIFiles/Docs/Aquaculture/IFB%20No.%2053%20%20National%20survey%20of%20Sea%20lice%20in%20Fish%20farms%202021.pdf",
+  `2020` = "https://www.marine.ie/sites/default/files/MIFiles/docs/Aquaculture/Irish%20Fisheries%20Bulletin%20No.%2052.pdf",
+  `2019` = "https://oar.marine.ie/bitstream/handle/10793/1590/Irish%20Fisheries%20Bulletin%20No.%2050.pdf?sequence=1&isAllowed=y",
+  `2018` = "https://www.marine.ie/sites/default/files/MIFiles/Images/Aquaculture/Irish%20Fisheries%20Bulletin%20No.%2049%202019.pdf",
+  `2017` = "https://oar.marine.ie/bitstream/handle/10793/1352/Irish%20Fisheries%20Bulletin%20No.%2048%20-%20Annual%20report%202018.pdf?sequence=5&isAllowed=y",
+  `2016` = "https://www.marine.ie/sites/default/files/MIFiles/Docs/Aquaculture/Irish%20Fisheries%20Bulletin%20No.%2047%20-%20Annual%20report%202017.pdf",
+  `2015` = "https://oar.marine.ie/bitstream/handle/10793/1146/Fisheries%20Bulletin%20No.%2046.pdf?sequence=1&isAllowed=y",
+  `2014` = "https://oar.marine.ie/bitstream/handle/10793/1078/Irish%20Fisheries%20Bulletin%20No%2045.pdf?sequence=1&isAllowed=y",
+  `2013` = "https://oar.marine.ie/bitstream/handle/10793/955/No%2044%202014%20National%20Survey%20of%20Sea%20Lice%20on%20Fish%20Farms%20in%20Ireland%202013%20%28with%20cover%29%20A4.pdf?sequence=1&isAllowed=y",
+  `2012` = "https://oar.marine.ie/bitstream/handle/10793/861/No.41.%202013.%20National%20Survey%20of%20sea%20lice%20on%20fish%20farms%20in%20Ireland%202012%20%28with%20cover%29%20A4.pdf?sequence=6&isAllowed=y",
+  `2011` = "https://oar.marine.ie/bitstream/handle/10793/776/Irish%20Fisheries%20Bulletin%20No40-National%20Survey%20of%20Sea%20Lice%202011.pdf?sequence=1&isAllowed=y",
+  `2010` = "https://oar.marine.ie/bitstream/handle/10793/104/IFB%20no.%2034.pdf?sequence=1&isAllowed=y",
+  `2009` = "https://oar.marine.ie/bitstream/handle/10793/32/Irish%20Fisheries%20Bulletin%20No%2033.pdf?sequence=1&isAllowed=y",
+  `2008` = "https://oar.marine.ie/bitstream/handle/10793/196/No32%20Irish%20Fisheries%20Bulletin.pdf?sequence=1&isAllowed=y"
 )
 
-extract_ireland_pdf <- function(file, start, end, too_few = "error", ...) {
-  file <- file.path("data/ireland/pdf/", file)
-  extract <- tabulapdf::extract_text(file, pages = start:end)
-  extract |>
-    str_split("\\n") |>
-    unlist() |>
-    str_trim() |>
-    str_subset(".+") |> # remove empty rows
-    str_subset("\\d{2}/\\d{2}/\\d{4} \\d") |>
-    str_extract("\\d{2}/\\d{2}/\\d{4}.*") |>
-    str_remove(" n[=|<]\\d{1,2}") |>
-    tibble(x = _) |>
-    separate_wider_regex(
-      cols = x,
-      too_few = too_few,
-      patterns = c(
-        date = "\\d{2}/\\d{2}/\\d{4}",
-        " ",
-        gravid_lep = "\\d+\\.\\d{2}",
-        " ",
-        total_lep = "\\d+\\.\\d{2}",
-        " ",
-        gravid_cal = "\\d+\\.\\d{2}",
-        " ",
-        total_cal = "\\d+\\.\\d{2}"
-      )
+# download pdfs
+for (link in links_ireland) {
+  base <- basename(link) |> str_remove("\\?.*$")
+  cat("trying", base, "\n")
+  if (!file.exists(paste0("data/ireland/pdf/", base))) {
+    tryCatch(
+      {
+        download.file(link, paste0("data/ireland/pdf/", base))
+      },
+      error = function(e) {
+        cat("error with download", link, "\n")
+      }
     )
+    Sys.sleep(1)
+  }
 }
-
-
-pdf_output <- pdfs |>
-  filter(good) |>
-  drop_na(start, end) |>
-  pmap(safely(extract_ireland_pdf))
-
-
-combined_pdf <- pdf_output |>
-  map("result") |>
-  list_rbind() |>
-  mutate(date = dmy(date), gravid_lep = as.numeric(gravid_lep))
-
-combined_pdf |>
-  ggplot(aes(x = date, y = gravid_lep)) +
-  geom_point()
-
-
-bad_optput <- pdfs |>
-  filter(good) |>
-  filter(
-    pdf_output |>
-      map("error") |>
-      map_lgl(negate(is.null))
-  ) |>
-  pmap(safely(extract_ireland_pdf))
-
-bad_optput
