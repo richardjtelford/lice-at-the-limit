bd50cf30ad868af0ac976ed4d9517ea237a06cf3
RichardJTelford <Richard.Telford@uib.no>
2025-03-20 19:22:47 +0100

    code to zip up latex assets

--- 0-make.R
+++ 0-make.R
@@ -8,3 +8,13 @@ tar_make()
 
 # visualize the pipeline
 tar_visnetwork()
+
+# zip tex file
+zip::zip(
+  "manuscript.zip",
+  c(
+    "lice-at-the-limit.tex",
+    list.files("lice-at-the-limit_files/figure-pdf/", full.names = TRUE),
+    "aft.cls"
+  )
+)
